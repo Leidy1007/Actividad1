b753ad77259a2b84f493fa771de2772b60ebeeb2
Leidy1007 <36243728+Leidy1007@users.noreply.github.com>
2018-02-19 22:35:34 -0500

    Add files via upload

---
+++ Base de datos/metrosinu/inicio.php
@@ -0,0 +1,126 @@
+<html>
+    <head>
+    <meta charset="UTF-6">
+
+        <title>El TIKE</title>
+
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="estilos/estilosmens.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="main.js"></script>
+    <script src="ir-arriba.js"></script>
+</head>
+<body>
+<span class="ir-arriba icon-arrow2"></span>
+<h4><font face="Neuropol" color="black">
+    <header>
+        <div class="menu_bar">
+            <a href="#" class="bt-menu"><span class="icon-menu2"></span>Menú</a>
+        </div>
+ 
+        <nav class="menu-fixed">
+<ul>
+
+<b> <center><FONT  SIZE=7 color=grey face="Comic Sans MS " >El TIKE</FONT></b></center>
+
+
+            </ul>
+              <br>
+        
+        </nav>
+        <br>
+        <br>
+
+
+        <br>
+        <center>
+
+
+
+            </center>
+    </header>
+
+      
+        <br>
+        
+
+    </head>
+     <body>
+    <center>
+           
+       <br><input type="submit" value="conductora" name="bt_Conductora">   
+<form action="conductora/registro.php" method="post" class="form-register" > 
+ <br>
+    <input type="text" name="nombre1" placeholder="primer nombre" class="input-48" required>   
+    <br>
+    <input type="text" name="nombre2" placeholder="segundo nombre" class="input-48" required>  
+    <br>
+    <input type="text" name="apellido1" placeholder="primer apellido" class="input-48" required>
+    <br>
+    <input type="text" name="apellido2" placeholder="segundo apellido" class="input-48" required>
+   <br>
+   <input type="num" name="identificacion" placeholder="cedula" class="input-48" required>
+   <br>
+   <input type="num" name="edad" placeholder="edad" class="input-48" required>
+   <br>
+    <input type="varchar" name="direccion" placeholder="Direccion" class="input-48" required>
+    <br>
+    <input type="submit" value="registrar" class="btn-enviar" required>
+</form>
+            
+      
+            <br><input type="submit" value="pasajeros " name="bt_pasajeros" >
+            <form action="pasajeros/registro.php" method="post" class="form-register" > 
+<br>
+<div class="contenedor-inputs">
+<input type="text" name="nombre1" placeholder="primer nombre" class="input-48" required>   
+    <br>
+    <input type="text" name="nombre2" placeholder="segundo nombre" class="input-48" required>  
+    <br>
+    <input type="text" name="apellido1" placeholder="primer apellido" class="input-48" required>
+    <br>
+    <input type="text" name="apellido2" placeholder="segundo apellido" class="input-48" required>
+   <br>
+   <input type="num" name="identificacion" placeholder="Identificacion" class="input-48" required>
+   <br>
+   <input type="num" name="edad" placeholder="edad" class="input-48" required>
+    <br>
+    <input type="varchar" name="direccion" placeholder="Direccion" class="input-48" required>
+    <br>
+    <input type="submit" value="registrar" class="btn-enviar" required>
+</form>
+    
+
+  <br>
+  <br>
+
+    <br><input type="submit" value="alimentadora " name="bt_alimentadora" >
+            <form action="alimentadora/registro.php" method="post" class="form-register" > 
+<br>
+<div class="contenedor-inputs">
+<input type="text" name="matricula" placeholder="matricula" class="input-48" required>   
+    <br>
+    <input type="text" name="modelo" placeholder="modelo" class="input-48" required>  
+       <br>
+    <input type="submit" value="registrar" class="btn-enviar" required>
+</form>
+
+
+<br>
+  <br>
+
+    <br><input type="submit" value="tike " name="bt_tike" >
+            <form action="tike/registro.php" method="post" class="form-register" > 
+<br>
+<div class="contenedor-inputs">
+<input type="text" name="codigo" placeholder="codigo" class="input-48" required>   
+    <br>
+       <br>
+    <input type="submit" value="registrar" class="btn-enviar" required>
+</form>
+           
+    
+<!-- saved from url=(0051)http://www.htmlpoint.com/guida/guida_frame_6_dx.htm -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252"></head><body bgcolor="white">
+
